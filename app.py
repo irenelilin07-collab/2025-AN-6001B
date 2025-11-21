1f3cd6cdd39d42ba7f8781f657f8a174445effc6
irenelilin07-collab <irenelilin07@gmail.com>
2025-11-21 07:47:18 +0000

    1

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 from flask import Flask, render_template, request
-#从 Flask 框架里导入三个工具
-
+import joblib
 app = Flask(__name__)
-
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -10,9 +8,21 @@ def index():
 
 @app.route("/main", methods=["GET", "POST"])
 def main():
-    q=request.form.get("q")
+    q = request.form.get("q")
     print(q)
     return render_template("main.html")
+
+@app.route("/dbs", methods=["GET", "POST"])
+def dbs():
+    return render_template("dbs.html")
+
+@app.route("/dbs_prediction", methods=["GET", "POST"])
+def dbs_prediction():
+    q = float(request.form.get("q"))
+    model = joblib.load("dbs.pkl")
+    r = model.predict([[q]])
+    return render_template("dbs_prediction.html",r=r[0][0])
+
 
 if __name__ == "__main__":
     app.run()
