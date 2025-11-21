d5ed56c04c2ce414e329679e1291bc6fc14866cb
irenelilin07-collab <irenelilin07@gmail.com>
2025-11-21 06:58:24 +0000

    1

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 from flask import Flask, render_template, request
+#从 Flask 框架里导入三个工具
 
 app = Flask(__name__)
+
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     return render_template("index.html")
+
+@app.route("/main", methods=["GET", "POST"])
+def main():
+    q=request.form.get("q")
+    print(q)
+    return render_template("main.html")
 
 if __name__ == "__main__":
     app.run()
